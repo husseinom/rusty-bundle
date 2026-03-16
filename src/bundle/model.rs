6858a4b0849c95f7fb148c14267f47ffe5bb4856
lysa <matmar.lysa@gmail.com>
2026-03-16 11:43:05 +0100

    Update dependencies and refactor Node structure for unique ID generation

--- src/bundle/model.rs
+++ src/bundle/model.rs
@@ -9,25 +9,28 @@ use chrono::{DateTime, Utc}; // for the date and time
 // fot he node structure 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Node {
-    pub id: String,      // string names ad example "NodeA" or "NodeB"
-    pub address: String, // the adresse of the node 
-    //TODO : is it an IP address ? or a name ?
-
+        pub id: Uuid, // unique identifier for the node
+        pub address: String, // IP address of the node
+        pub port: u16, // port the node listens on
+        pub peers: Vec<Uuid>, // IDs of known peer nodes
 }
 
 // implementation of the node struct
 impl Node {
-    pub fn new(id: &str, address: &str) -> Self {
+    pub fn new(address: &str, port: u16, peers: Vec<Uuid>) -> Self {
         Node {
-            id: id.to_string(),
+            id: Uuid::new_v4(),
             address: address.to_string(),
+            port,
+            peers,
         }
-    }
+       
+}
 }
 
 // fot the MsgStatus we use an enumeration to represent the different status of the bundle during its lifecycle
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub enum MsgStatus { // TODO: is it correct for the moment to use these for the status of the bundle ?
+pub enum MsgStatus { 
     Pending,    // the bundle is created but not yet sent
     InTransit,  // the bundle is on the way to the destination
     Delivered,  // the bundle has been delivered to the destination
@@ -42,14 +45,12 @@ pub struct Bundle {
     pub destination: Node,           // the destination node of the bundle
     pub timestamp: DateTime<Utc>,    // date and time of the bundle creation
     pub ttl: u64,                    // time to live in seconds, after which the bundle is considered expired
-    //TODO: is it correct to use u64 for the ttl ? or should we use a different type ?
     pub msg: String,                 // the message content of the bundle
     pub shipment_status: MsgStatus,  // the current status of the bundle during its lifecycle
 }
 //implementation of the bundle struct
 impl Bundle {
     pub fn new(source: Node, destination: Node, msg: String, ttl: u64) -> Self { // for the new bundle we need the source, destination, message and ttl 
-        //TODO: is it correct htat i added the ttl for the time to live of the bundle or we don't need it in the structure 
         Bundle {
             id: Uuid::new_v4().to_string(), // generate a unique id for the bundle using uuid version 4 and convert it to string before storing it in the json file
                                             // more information inside the instructions.md file in the feat21-imple…D-generation section
