fcaf859a242c1270d9dc22362ea37598bdb93a8f
lysa <matmar.lysa@gmail.com>
2026-03-20 10:45:45 +0100

    Remove commented-out send_message function from client.rs

--- src/network/client.rs
+++ src/network/client.rs
@@ -5,28 +5,6 @@ use serde_json::json;
 use uuid::Uuid;
 use std::collections::HashMap;
 
-/// Connects to a peer node and sends a message
-// pub fn send_message(address: &str, message: &str) {
-//     // Try to connect to the peer
-//     match TcpStream::connect(address) {
-//         Ok(mut stream) => {
-//             println!("[Client] Connected to {}", address);
-//             stream.write_all(message.as_bytes())
-//                 .expect("Failed to send message");
-//             println!("[Client] Message sent: {}", message);
-
-//             // Read the response from the server
-//             let mut buffer = [0; 1024];
-//             stream.read(&mut buffer)
-//                 .expect("Failed to read response");
-//             let response = String::from_utf8_lossy(&buffer);
-//             println!("[Client] Server response: {}", response);
-//         }
-//         Err(e) => {
-//             println!("[Client] Could not connect to {}: {}", address, e);
-//         }
-//     }
-// }
 
 fn getNode(node: &Node) -> Node {
     return node.clone(); //TODO: see if the clone method is good to get all the informations of the node 
